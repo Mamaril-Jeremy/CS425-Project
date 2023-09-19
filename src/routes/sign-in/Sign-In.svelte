7a9a7ac9f87bf71d73758e4b1ca4557bf058ddbd
Mamaril-Jeremy <jmamaril@nevada.unr.edu>
2023-09-19 15:03:23 -0700

    commit

---
+++ src/routes/sign-in/Sign-In.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { Link } from "svelte-routing";
+</script>
+
+<main>
+<p>Welcome to Plato</p>
+<p>Username: </p>
+<input type="text">
+<p>Password: </p>
+<input type="text">
+<p></p>
+<button><Link to="/home">Sign In</Link></button>
+</main>
+
+ <style>
+    button{
+        background:#03a9f4;
+    }
+ </style>
