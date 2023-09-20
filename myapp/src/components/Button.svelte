280ec2f168a257339c7bd758898014b164d9758c
Mamaril-Jeremy <jmamaril@nevada.unr.edu>
2023-09-19 20:01:31 -0700

    fixed a lot

---
+++ myapp/src/components/Button.svelte
@@ -0,0 +1,53 @@
+<!-- <script>
+	let buttonProps = {class:[$$restProps.class]}
+</script>
+	
+<button on:click
+	{...buttonProps}>
+	<slot/>
+</button>
+
+<style>
+	.connect{
+		color:green;
+        position: absolute;
+        top: 400px;
+        left: 1300px;
+        width: 100px;
+        height: 40px;
+	}
+	.green{
+		font-size:1em;
+		padding:0.1em;
+	}
+    .pass{
+		color:red;
+        position: absolute;
+        top: 400px;
+        left: 200px;
+        width: 100px;
+        height: 40px;
+	}
+	.red{
+		font-size:1em;
+		padding:0.1em;
+	}
+</style> -->
+
+
+<style>
+    .button {
+        top: 100px;
+        left: 0px;
+    }
+</style>
+
+<script>
+    import { Button, GradientButton } from 'flowbite-svelte';
+</script>
+
+<div class="button">
+    <Button>
+        Connect 
+    </Button>
+</div>
---
+++ myapp/src/components/Card.svelte
@@ -0,0 +1,61 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">   
+</head>
+<body>
+    <div class="name">Jeremy Mamaril</div>
+    <div class="card">
+        <div class="section">Resume</div>
+        <div class="section">Interests</div>
+        <div class="section">Accolades</div>
+        <div class="section">Education</div>
+        <div class="circle"></div>
+    </div>
+</body>
+
+<style>
+    .card {
+        width: 600px;
+        height: 400px;
+        border: 1px solid #000;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        overflow: hidden;
+        position: absolute;
+        top: 350px;
+        left: 750px;
+        transform: translate(-50%, -50%);
+    }
+
+    .name{
+        position: relative;
+        top: 100px;
+        left: 0px;
+        align-items: center;
+        font-size: 18px;
+    }
+
+    .section {
+        width: 50%;
+        height: 50%;
+        box-sizing: border-box;
+        border: 1px solid #000;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    
+    .circle {
+        width: 30%; 
+        height: 40%; 
+        background-color: #FFFFFF; 
+        border-radius: 50%; 
+        border: 1px solid #000;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+</style>
