4a5520eb161bddf6ce0830dfc22f2d30a077b3e7
Mamaril-Jeremy <jmamaril@nevada.unr.edu>
2023-09-28 13:18:21 -0700

    new

--- myapp/src/routes/sign-in/Sign-In.svelte
+++ myapp/src/routes/sign-in/Sign-In.svelte
@@ -2,24 +2,30 @@
   import { Card, Button, Label, Input, Checkbox } from 'flowbite-svelte';
 </script>
 
-<Card class="w-72 max-w-none">
-  <form class="flex flex-col space-y-6" action="/">
-    <h3 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
-    <Label class="space-y-2">
-      <span>Email</span>
-      <Input type="email" name="email" placeholder="name@company.com" required />
-    </Label>
-    <Label class="space-y-2">
-      <span>Your password</span>
-      <Input type="password" name="password" placeholder="•••••" required />
-    </Label>
-    <div class="flex items-start">
-      <Checkbox>Remember me</Checkbox>
-      <a href="/" class="ml-auto text-sm text-primary-700 hover:underline dark:text-primary-500"> Lost password? </a>
-    </div>
-    <Button type="submit" class="w-full">Login to your account</Button>
-    <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-      Not registered? <a href="/" class="text-primary-700 hover:underline dark:text-primary-500"> Create account </a>
-    </div>
-  </form>
-</Card>
+<div class="card-container">
+  <Card class="w-72 max-w-none">
+    <form class="flex flex-col space-y-6" action="/">
+      <h3 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
+      <Label class="space-y-2">
+        <span>Email</span>
+        <Input type="email" name="email" placeholder="name@company.com" required />
+      </Label>
+      <Label class="space-y-2">
+        <span>Your password</span>
+        <Input type="password" name="password" placeholder="•••••" required />
+      </Label>
+      <div class="flex items-start">
+        <Checkbox>Remember me</Checkbox>
+        <a href="/" class="ml-auto text-sm text-primary-700 hover:underline dark:text-primary-500"> Lost password? </a>
+      </div>
+      <Button type="submit" class="w-full">Login to your account</Button>
+      <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+        Not registered? <a href="/" class="text-primary-700 hover:underline dark:text-primary-500"> Create account </a>
+      </div>
+    </form>
+  </Card>
+</div>
+
+<style>
+
+</style>
