54046cd017698c301ab0668979f89c718e7ba635
Mamaril-Jeremy <jmamaril@nevada.unr.edu>
2023-09-29 14:30:02 -0700

    new

--- myapp/src/routes/sign-in/Sign-In.svelte
+++ myapp/src/routes/sign-in/Sign-In.svelte
@@ -2,32 +2,35 @@
   import { Card, Button, Label, Input, Checkbox } from 'flowbite-svelte';
 </script>
 
-<p style="background-image: url('placeholderbackground_login.png');">
-
-<div class="card-container">
-  <Card class="w-72 max-w-none">
-    <form class="flex flex-col space-y-6" action="/">
-      <h3 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
-      <Label class="space-y-2">
-        <span>Email</span>
-        <Input type="email" name="email" placeholder="name@company.com" required />
-      </Label>
-      <Label class="space-y-2">
-        <span>Your password</span>
-        <Input type="password" name="password" placeholder="•••••" required />
-      </Label>
-      <div class="flex items-start">
-        <Checkbox>Remember me</Checkbox>
-        <a href="/" class="ml-auto text-sm text-blue-600 hover:underline dark:text-primary-500"> Lost password? </a>
-      </div>
-      <Button href="/home" type="submit" class="w-full bg-blue-600 hover:opacity-75 hover:bg-blue-600">Login to your account</Button>
-      <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-        Not registered? <a href="/" class="text-blue-600 hover:underline dark:text-primary-500"> Create account </a>
-      </div>
-    </form>
-  </Card>
+<div class="body-background h-screen w-screen flex items-center justify-center">
+  <div class="card-container">
+    <Card class="w-72 max-w-none">
+      <form class="flex flex-col space-y-6" action="/">
+        <h3 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
+        <Label class="space-y-2">
+          <span>Email</span>
+          <Input type="email" name="email" placeholder="name@company.com" required />
+        </Label>
+        <Label class="space-y-2">
+          <span>Your password</span>
+          <Input type="password" name="password" placeholder="•••••" required />
+        </Label>
+        <div class="flex items-start">
+          <Checkbox>Remember me</Checkbox>
+          <a href="/" class="ml-auto text-sm text-blue-600 hover:underline dark:text-primary-500"> Lost password? </a>
+        </div>
+        <Button href="/home" type="submit" class="w-full bg-blue-600 hover:opacity-75 hover:bg-blue-600">Login to your account</Button>
+        <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+          Not registered? <a href="/" class="text-blue-600 hover:underline dark:text-primary-500"> Create account </a>
+        </div>
+      </form>
+    </Card>
+  </div>
 </div>
-
 <style>
-
+  .body-background {
+    background-image: url('../../assets/placeholderbackground_login.png');
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
 </style>
