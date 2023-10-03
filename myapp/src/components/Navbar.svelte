9e57aead0ed0c3023813af0758214c520e501563
Mamaril-Jeremy <jmamaril@nevada.unr.edu>
2023-10-03 13:12:16 -0700

    new

--- myapp/src/components/Navbar.svelte
+++ myapp/src/components/Navbar.svelte
@@ -1,7 +1,28 @@
 <script>
-    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button, Input } from 'flowbite-svelte';
-    import { SearchOutline } from 'flowbite-svelte-icons';
-</script>
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, GradientButton } from 'flowbite-svelte';
+  </script>
+  
+
+<div class="navbar-container">
+<Navbar let:hidden let:toggle>
+    <NavBrand href="/">
+        <img src="/images/flowbite-svelte-icon-logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+        <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Plato</span>
+    </NavBrand>
+    <div class="flex md:order-2">
+        <GradientButton color="blue" href="/sign-in">Sign In</GradientButton>
+        <NavHamburger on:click={toggle} />
+    </div>
+    <NavUl {hidden} class="order-1">
+        <NavLi href="/home"><span class="hover:text-blue-600">Home</span></NavLi>
+        <NavLi href="/chat"><span class="hover:text-blue-600">Chat</span></NavLi>
+        <NavLi href="/connections"><span class="hover:text-blue-600">Connections</span></NavLi>
+        <NavLi href="/events"><span class="hover:text-blue-600">Events</span></NavLi>
+        <NavLi href="/profile"><span class="hover:text-blue-600">Profile</span></NavLi>
+        <NavLi href="/about"><span class="hover:text-blue-600">About</span></NavLi>
+    </NavUl>
+    </Navbar>
+</div> 
 
 <style>
     /* Apply CSS styles to make Navbar stick to the top */
@@ -14,30 +35,6 @@
     }
 </style>
 
-<div class="navbar-container">
-    <Navbar let:hidden let:toggle>
-        <NavBrand href="/">
-            <img src="/images/flowbite-svelte-icon-logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-            <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Plato</span>
-        </NavBrand>
-        <div class="flex md:order-2">
-            <Button color="none" data-collapse-toggle="mobile-menu-3" aria-controls="mobile-menu-3" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1">
-                <SearchOutline class="w-5 h-5" />
-            </Button>
-            <div class="hidden relative md:block">
-                <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                    <SearchOutline class="w-4 h-4" />
-                </div>
-                <Input id="search-navbar" class="pl-10" placeholder="Search..." />
-            </div>
-            <NavHamburger on:click={toggle} />
-        </div>
-        <NavUl {hidden}>
-            <NavLi href="/home" active={true}>Home</NavLi>
-            <NavLi href="/chat">Chat</NavLi>
-            <NavLi href="/connections" active={true}>Connections</NavLi>
-            <NavLi href="/events">Event</NavLi>
-            <NavLi href="/profile" active={true}>Profile</NavLi>
-        </NavUl>
-    </Navbar>
-</div>
+
+
+  
--- myapp/src/routes/sign-in/Sign-In.svelte
+++ myapp/src/routes/sign-in/Sign-In.svelte
@@ -1,25 +1,36 @@
 <script>
   import { Card, Button, Label, Input, Checkbox } from 'flowbite-svelte';
+
+  let email = '';
+  let password = '';
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // Perform form data processing, validation, and submission logic here
+
+    // Redirect the user to the home page after successful login
+    window.location.href = '/home';
+  };
 </script>
 
 <div class="body-background h-screen w-screen flex items-center justify-center">
   <div class="card-container">
     <Card class="w-72 max-w-none">
-      <form class="flex flex-col space-y-6" action="/">
+      <form class="flex flex-col space-y-6" on:submit={handleSubmit}>
         <h3 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
         <Label class="space-y-2">
           <span>Email</span>
-          <Input type="email" name="email" placeholder="name@company.com" required />
+          <Input type="email" bind:value={email} placeholder="name@company.com" required />
         </Label>
         <Label class="space-y-2">
           <span>Your password</span>
-          <Input type="password" name="password" placeholder="•••••" required />
+          <Input type="password" bind:value={password} placeholder="•••••" required />
         </Label>
         <div class="flex items-start">
           <Checkbox>Remember me</Checkbox>
           <a href="/" class="ml-auto text-sm text-blue-600 hover:underline dark:text-primary-500"> Lost password? </a>
         </div>
-        <Button href="/home" type="submit" class="w-full bg-blue-600 hover:opacity-75 hover:bg-blue-600">Login to your account</Button>
+        <Button type="submit" class="w-full bg-blue-600 hover:opacity-75 hover:bg-blue-600">Login to your account</Button>
         <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
           Not registered? <a href="/" class="text-blue-600 hover:underline dark:text-primary-500"> Create account </a>
         </div>
@@ -27,6 +38,7 @@
     </Card>
   </div>
 </div>
+
 <style>
   .body-background {
     background-image: url('../../assets/placeholderbackground_login.png');
