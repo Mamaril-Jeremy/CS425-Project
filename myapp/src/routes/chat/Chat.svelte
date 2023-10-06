a1ea6e0fbca5a6aeef1d588c6393f70b5204a772
cao-richard <70532763+cao-richard@users.noreply.github.com>
2023-10-06 12:42:13 -0700

    Update Chat.svelte

--- myapp/src/routes/chat/Chat.svelte
+++ myapp/src/routes/chat/Chat.svelte
@@ -8,6 +8,9 @@
     <Sidebar>
         <SidebarWrapper>
             <SidebarGroup>
+                <div class = "Title">
+                    <p>Connections</p>
+                </div>
                 <SidebarItem label="Mark Marsala" href="/">
                     <svelte:fragment slot="icon">
                     <ChartPieSolid class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
@@ -32,31 +35,84 @@
     <div class = "chatbox">
         <div class = "message-container">
         </div>
+        <div class = "textbox">
+            <input type="text" placeholder="Enter text here"/>
+        </div>
+        <button>Send</button>
     </div>
 </div>
 
 <style>
     .Sidebar{
         position : fixed;
-        top : 3rem;
+        top : 5rem;
         left : 0rem;
         height : 44rem;
-        box-shadow: rgba(255,255,255,1) 0px 3px 8px;
+        box-shadow: rgba(0,0,0,25) 0px 3px 8px;
+        background: white;
     }
     .message-container{
         overflow-y: scroll;
-        height : 41rem;
+        height : 37rem;
     }
     .chatbox{
         width : 74rem;
         height : 41rem;
-        /* margin:auto; */
         position:fixed;
-        top: 5rem;
+        top: 5.5rem;
         left: 16rem;
         border-radius: .5rem;
         box-shadow: rgba(0,0,0,0.25) 0px 3px 8px;
+        background: white;
+        z-index: 500;
+    }  
+    
+    button
+    {
+        position: absolute;
+        bottom: 0.5rem;
+        right: 0.5rem;
+        width : 100px;
+        height : 2.75rem;
+        background-color: rgb(250, 144, 23);
+        color:rgb(0, 0, 0); 
+        cursor:pointer;
+        border-radius: .25rem;
+        border:black;
+        transition: all ease .25s;
+        z-index: 1000;
+    } 
+
+    button:hover
+    {
+        z-index: 1000;
+        background-color: rgb(255, 203, 144);
+    } 
+
+    .textbox
+    {
+        position : absolute;
+        bottom: 0.5rem;
+        right: 48rem;
+        width : 400px;
+        box-sizing: border-box;
+        font-size: 16px;
+        z-index: 1000;
+        background-color: black;
+        border : 1px solid black;
+        padding: 0;
+        margin : 0;
     }
+    .textbox input[type="text"] {
+        width: 1050px;
+        color: black;
+    }
+
+    .Title{
+        color:black;
+        font-size: 30px;
+    }
+
     /* .container{
         width: 100%;
     } */
