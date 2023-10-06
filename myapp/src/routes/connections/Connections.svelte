35ccc12b1c7e5bb482a71ef46a0b3bde5fd2eda3
Mamaril-Jeremy <jmamaril@nevada.unr.edu>
2023-10-06 14:15:34 -0700

    new

--- myapp/src/routes/connections/Connections.svelte
+++ myapp/src/routes/connections/Connections.svelte
@@ -1,1 +1,51 @@
-<p>Connections</p>
+<body>
+    <div class="container">
+        <div class="quadrant"></div>
+        <div class="quadrant"></div>
+        <div class="quadrant"></div>
+        <div class="quadrant"></div>
+    </div>
+</body>
+
+<style>
+    body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+}
+
+.container {
+    width: 200px;
+    height: 200px;
+    position: relative;
+}
+
+.quadrant {
+    width: 50%;
+    height: 50%;
+    position: absolute;
+}
+
+.quadrant:nth-child(1) {
+    top: 0;
+    left: 0;
+}
+
+.quadrant:nth-child(2) {
+    top: 0;
+    left: 50%;
+}
+
+.quadrant:nth-child(3) {
+    top: 50%;
+    left: 0;
+}
+
+.quadrant:nth-child(4) {
+    top: 50%;
+    left: 50%;
+}
+
+</style>
