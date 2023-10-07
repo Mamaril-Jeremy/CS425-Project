e2293def24495c8e6478ccdff1575c58396c089e
Mamaril-Jeremy <jmamaril@nevada.unr.edu>
2023-10-06 19:24:01 -0700

    new

--- myapp/src/routes/home/Home.svelte
+++ myapp/src/routes/home/Home.svelte
@@ -14,15 +14,6 @@
   </Link>
 </div>
 
-<!-- Button tests -->
-<GradientButton color="blue">Blue</GradientButton>
-<GradientButton color="green">Green</GradientButton>
-<GradientButton color="cyan">Cyan</GradientButton>
-<GradientButton color="teal">Teal</GradientButton>
-<GradientButton color="lime">Lime</GradientButton>
-<GradientButton color="red">Red</GradientButton>
-<GradientButton color="pink">Pink</GradientButton>
-
 <style>
   .button {
     left: 300px;
