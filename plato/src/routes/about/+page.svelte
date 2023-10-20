a1612dcb821f40f50ee136562649ebf1fb0a168c
Mamaril-Jeremy <jmamaril@nevada.unr.edu>
2023-10-20 14:21:46 -0700

    new

--- plato/src/routes/about/+page.svelte
+++ plato/src/routes/about/+page.svelte
@@ -1,99 +1,99 @@
 <script>
-    //import img from "$lib/assets/RenoRodeo.jpg";
+  //import img from "$lib/assets/RenoRodeo.jpg";
 </script>
 
 <style>
-    /* Base styles for the About page */
-    .about-page {
-      font-size: 16px;
-      line-height: 1.5;
-      margin-bottom: 20px;
-    }
-  
-    h1, h2 {
-      font-size: 24px;
-      margin-bottom: 10px;
-    }
-  
-    /* Apply a scale transformation and cursor change on hover for the "Plato" text */
-    .about-page .gradient-text-blue:hover {
-      transform: scale(1.05); /* Enlarge on hover */
-      cursor: pointer; /* Change cursor on hover to indicate interaction */
-    }
-  
-    /* Styling for text with a gradient effect */
-    .gradient-text {
-      background: linear-gradient(rgb(80, 101, 168), rgb(11, 20, 119));
-      -webkit-background-clip: text;
-      background-clip: text;
-      color: transparent;
-    }
-  
-    .gradient-text-blue {
-      background: linear-gradient(rgb(73, 131, 250), rgb(58, 103, 193));
-      -webkit-background-clip: text;
-      background-clip: text;
-      color: transparent;
-    }
-  
-    /* Styling for images */
-    .about-image {
-      max-width: 100%; /* Ensure images don't exceed their container width */
-      display: block; /* Center the images within the container */
-      margin: 20px auto; /* Add margin for spacing */
-      box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); /* Add a subtle shadow to the images */
-    }
-  </style>
-  
-  <!-- About Page Content -->
-  <div class="about-page">
-    <br>
-    <br>
-  
-    <!-- Hero Section -->
-    <br>
-    <h1>Welcome to <a href="/home" class="gradient-text-blue">Plato</a>, the platform that opens the doors to your personal and professional growth journey.</h1> 
-    <p>We're not just another networking site; we are your trusted partner in connecting you with the wisdom and guidance you need to thrive in your chosen field on a mentor-mentee basis.</p>
-    <!-- <img src={img} alt="Illustration of mentorship" class="about-image"> -->
-    <br>
-    <br>
-  
-    <!-- "Why 'Plato'?" Section -->
-    <h2><strong class="gradient-text">Why "Plato"?</strong></h2>
-    <br>
-    <p>In the grand tradition of the ancient philosopher <a href="/home" class="gradient-text-blue">Plato</a>, who artfully navigated the intricacies of life's profound inquiries while guiding his devoted disciples, imagine having a seasoned mentor by your side, guiding you through the twists and turns of your career or academic journey. At <a href="/home" class="gradient-text-blue">Plato</a>, mentorship isn't just a feature; it's at the heart of what we do. We believe that the right mentor can be your compass, helping you navigate the path to success. Whether you're a student aiming to conquer challenging subjects or a professional seeking to excel in your industry, we've got you covered.</p>
+  /* Base styles for the About page */
+  .about-page {
+    font-size: 16px;
+    line-height: 1.5;
+    margin-bottom: 20px;
+  }
 
-    <br>
-    <br>
-  
-    <!-- "Connecting Dreams and Expertise" Section -->
-    <h2><strong class="gradient-text">Connecting Dreams and Expertise</strong></h2>
-    <br>
-    <p><a href="/home" class="gradient-text-blue">Plato</a> is where dreams meet expertise. We're the bridge that brings together passionate learners and seasoned professionals, creating a dynamic synergy that propels you towards greatness. No more wondering where to find the right guidance or feeling lost in a sea of information. Here, you will find mentors who have walked the path you're treading eager to share their wisdom with you.</p>
+  h1, h2 {
+    font-size: 24px;
+    margin-bottom: 10px;
+  }
 
-    <br>
-    <br>
-  
-    <!-- "Personalized Mentorship" Section -->
-    <h2><strong class="gradient-text">Personalized Mentorship</strong></h2>
-    <br>
-    <p>What sets <a href="/home" class="gradient-text-blue">Plato</a> apart is our unrelenting commitment to the art of personalization. We curate connections based on your unique goals, interests, and aspirations. Our cutting-edge algorithm ensures that you are paired with mentors who possess an intimate understanding of your specific needs, capable of providing tailored, precise support, much akin to a mentor who not only speaks your language but also understands your ambitions in intricate detail.</p>
+  /* Apply a scale transformation and cursor change on hover for the "Plato" text */
+  .about-page .gradient-text-blue:hover {
+    transform: scale(1.05); /* Enlarge on hover */
+    cursor: pointer; /* Change cursor on hover to indicate interaction */
+  }
 
-    <br>
-    <br>
-  
-    <!-- "Trust" Section -->
-    <h2><strong class="gradient-text">Trust</strong></h2>
-    <br>
-    <p><a href="/home" class="gradient-text-blue">Trust</a> is the cornerstone of <a href="/home" class="gradient-text-blue">Plato</a>. We have painstakingly constructed a secure and transparent platform that not only safeguards your privacy but also guarantees the authenticity of our users. Say farewell to the specter of impersonation and fraudulent actors; <a href="/home" class="gradient-text-blue">Plato</a> stands as your sanctuary for forging genuine and meaningful connections.</p>
+  /* Styling for text with a gradient effect */
+  .gradient-text {
+    background: linear-gradient(rgb(80, 101, 168), rgb(11, 20, 119));
+    -webkit-background-clip: text;
+    background-clip: text;
+    color: transparent;
+  }
 
-    <br>
-    <br>
-  
-    <!-- "Join the Plato Community Today" Section -->
-    <h2><strong class="gradient-text">Join the Plato Community Today</strong></h2>
-    <p>Enter the thriving community of learners and mentors. Whether you are a student eager to conquer the most formidable academic challenges or a professional steadfastly committed to propelling your career to loftier heights, <a href="/home" class="gradient-text-blue">Plato</a> is your gateway to unlocking untapped reservoirs of potential.</p>
+  .gradient-text-blue {
+    background: linear-gradient(rgb(73, 131, 250), rgb(58, 103, 193));
+    -webkit-background-clip: text;
+    background-clip: text;
+    color: transparent;
+  }
 
-    <br>
-    <p>Start your journey today and discover how mentorship can propel you to the pinnacles of success. Your voyage towards success commences here, within the ambitions, mentorship, and trust of <a href="/home" class="gradient-text-blue">Plato</a>.</p>
-  </div>
+  /* Styling for images */
+  .about-image {
+    max-width: 100%; /* Ensure images don't exceed their container width */
+    display: block; /* Center the images within the container */
+    margin: 20px auto; /* Add margin for spacing */
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); /* Add a subtle shadow to the images */
+  }
+</style>
+
+<!-- About Page Content -->
+<div class="about-page">
+  <br>
+  <br>
+
+  <!-- Hero Section -->
+  <br>
+  <h1>Welcome to <a href="/home" class="gradient-text-blue">Plato</a>, the platform that opens the doors to your personal and professional growth journey.</h1> 
+  <p>We're not just another networking site; we are your trusted partner in connecting you with the wisdom and guidance you need to thrive in your chosen field on a mentor-mentee basis.</p>
+  <!-- <img src={img} alt="Illustration of mentorship" class="about-image"> -->
+  <br>
+  <br>
+
+  <!-- "Why 'Plato'?" Section -->
+  <h2><strong class="gradient-text">Why "Plato"?</strong></h2>
+  <br>
+  <p>In the grand tradition of the ancient philosopher <a href="/home" class="gradient-text-blue">Plato</a>, who artfully navigated the intricacies of life's profound inquiries while guiding his devoted disciples, imagine having a seasoned mentor by your side, guiding you through the twists and turns of your career or academic journey. At <a href="/home" class="gradient-text-blue">Plato</a>, mentorship isn't just a feature; it's at the heart of what we do. We believe that the right mentor can be your compass, helping you navigate the path to success. Whether you're a student aiming to conquer challenging subjects or a professional seeking to excel in your industry, we've got you covered.</p>
+
+  <br>
+  <br>
+
+  <!-- "Connecting Dreams and Expertise" Section -->
+  <h2><strong class="gradient-text">Connecting Dreams and Expertise</strong></h2>
+  <br>
+  <p><a href="/home" class="gradient-text-blue">Plato</a> is where dreams meet expertise. We're the bridge that brings together passionate learners and seasoned professionals, creating a dynamic synergy that propels you towards greatness. No more wondering where to find the right guidance or feeling lost in a sea of information. Here, you will find mentors who have walked the path you're treading eager to share their wisdom with you.</p>
+
+  <br>
+  <br>
+
+  <!-- "Personalized Mentorship" Section -->
+  <h2><strong class="gradient-text">Personalized Mentorship</strong></h2>
+  <br>
+  <p>What sets <a href="/home" class="gradient-text-blue">Plato</a> apart is our unrelenting commitment to the art of personalization. We curate connections based on your unique goals, interests, and aspirations. Our cutting-edge algorithm ensures that you are paired with mentors who possess an intimate understanding of your specific needs, capable of providing tailored, precise support, much akin to a mentor who not only speaks your language but also understands your ambitions in intricate detail.</p>
+
+  <br>
+  <br>
+
+  <!-- "Trust" Section -->
+  <h2><strong class="gradient-text">Trust</strong></h2>
+  <br>
+  <p><a href="/home" class="gradient-text-blue">Trust</a> is the cornerstone of <a href="/home" class="gradient-text-blue">Plato</a>. We have painstakingly constructed a secure and transparent platform that not only safeguards your privacy but also guarantees the authenticity of our users. Say farewell to the specter of impersonation and fraudulent actors; <a href="/home" class="gradient-text-blue">Plato</a> stands as your sanctuary for forging genuine and meaningful connections.</p>
+
+  <br>
+  <br>
+
+  <!-- "Join the Plato Community Today" Section -->
+  <h2><strong class="gradient-text">Join the Plato Community Today</strong></h2>
+  <p>Enter the thriving community of learners and mentors. Whether you are a student eager to conquer the most formidable academic challenges or a professional steadfastly committed to propelling your career to loftier heights, <a href="/home" class="gradient-text-blue">Plato</a> is your gateway to unlocking untapped reservoirs of potential.</p>
+
+  <br>
+  <p>Start your journey today and discover how mentorship can propel you to the pinnacles of success. Your voyage towards success commences here, within the ambitions, mentorship, and trust of <a href="/home" class="gradient-text-blue">Plato</a>.</p>
+</div>
