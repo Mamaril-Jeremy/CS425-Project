5a432c35b4d84733a540ecc263343174e44d7673
Mamaril-Jeremy <jmamaril@nevada.unr.edu>
2023-10-20 14:03:18 -0700

    new home and chat

--- plato/src/routes/chat/+page.svelte
+++ plato/src/routes/chat/+page.svelte
@@ -39,16 +39,7 @@
             <input type="text" placeholder="Enter message here"/>
         </div>
         <div class ="button"><GradientButton color="blue">Send</GradientButton></div>
-        <Banner id="default-banner" position="absolute">
-            <p class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
-              <span class="inline-flex p-1 mr-3 bg-gray-200 rounded-full dark:bg-gray-600">
-                <span class="sr-only">Light bulb</span>
-              </span>
-              <span>
-                New brand identity has been launched for the <a href="https://flowbite.com" class="inline font-medium text-primary-600 underline dark:text-primary-500 underline-offset-2 decoration-600 dark:decoration-500 decoration-solid hover:no-underline"> Flowbite Library </a>
-              </span>
-            </p>
-          </Banner>
+      
     </div>
 </div>
 
