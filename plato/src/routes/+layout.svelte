5fb92df9fef96eaa2f6322d44dfde2d8414f8867
Mamaril-Jeremy <jmamaril@nevada.unr.edu>
2023-10-20 14:54:18 -0700

    fixed more on our meeting

--- plato/src/routes/+layout.svelte
+++ plato/src/routes/+layout.svelte
@@ -1,13 +1,7 @@
 <script>
+	import NavbarIn from "../lib/components/NavbarIn.svelte";
   import "../app.postcss";
-  import NavbarIn from "$lib/components/NavbarIn.svelte";
-  // import Logo from '$lib/assets/plato_logo.png';
 </script>
-
-<!-- <svelte:head>
-      <title>Plato</title>
-      <link rel="icon" href={Logo} />
-</svelte:head> -->
 
 <NavbarIn/>
 <slot />
