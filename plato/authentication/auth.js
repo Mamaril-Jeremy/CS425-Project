08067e53b8c29d5ad5d29b614ca647f03684c2ab
Mamaril-Jeremy <jmamaril@nevada.unr.edu>
2023-10-30 16:55:51 -0700

    fully resolved in and out navigation and polished authentication

--- plato/authentication/auth.js
+++ plato/authentication/auth.js
@@ -1,39 +1,28 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth } from 'src/lib/index'
-import { goto } from '$app/navigation'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from 'src/lib/index';
+import { goto } from '$app/navigation';
 
-let email;
-let password;
-let success;
+let email = ''; 
+let password = ''; 
+export let success = false;
 
 createUserWithEmailAndPassword(auth, email, password)
   .then(() => {
-    goto('src/routes/profile/+page.svelte')
+    goto('src/routes/profile/+page.svelte');
+    success = true;
   })
   .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    if (errorCode == 'auth/weak-password') {
-      alert('The password is too weak.');
-    } else {
-      alert(errorMessage);
-    }
-    console.log(errorCode, errorMessage);
-    success = false;
+    const errorMessage = 'Registration failed. Please try again later.';
+    alert(errorMessage);
+    console.error(error);
   });
-
 
 signInWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
     const user = userCredential.user;
   })
   .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    if (errorCode === 'auth/wrong-password') {
-      alert('Wrong password.');
-    } else {
-      alert(errorMessage);
-    }
-    console.log(error);
+    const errorMessage = 'Login failed. Please check your credentials and try again.';
+    alert(errorMessage);
+    console.error(error);
   });
