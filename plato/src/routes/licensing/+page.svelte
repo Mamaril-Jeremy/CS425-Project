b33c49e95fc5f2ff0c8702c8e0502dc23ec6cf06
MichaelRNia <michael.nia@nevada.unr.edu>
2023-11-17 14:52:57 -0800

    Preparing footer pages to push

--- plato/src/routes/licensing/+page.svelte
+++ plato/src/routes/licensing/+page.svelte
@@ -14,20 +14,8 @@
     text-align: center;
   }
 
-  .licensing-page .gradient-text-blue:hover {
-    transform: scale(1.05);
-    cursor: pointer;
-  }
-
   .gradient-text {
     background: linear-gradient(rgb(80, 101, 168), rgb(11, 20, 119));
-    -webkit-background-clip: text;
-    background-clip: text;
-    color: transparent;
-  }
-
-  .gradient-text-blue {
-    background: linear-gradient(rgb(73, 131, 250), rgb(58, 103, 193));
     -webkit-background-clip: text;
     background-clip: text;
     color: transparent;
