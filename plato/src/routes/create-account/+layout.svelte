5618c68e324bdc09f341ba5df307fc1d9fcc6359
markmarsala <markmarsala@nevada.unr.edu>
2023-11-24 15:24:16 -0800

    User Sign Up Working!

--- plato/src/routes/create-account/+layout.svelte
+++ plato/src/routes/create-account/+layout.svelte
@@ -1,18 +1,1 @@
-<script>
-    //import "../app.postcss";
-    import { onMount } from 'svelte'
-    import { auth } from '../../lib/firebase/firebase.client'
-    import { authStore } from '../../stores/authStore'
-  
-    onMount(() => {
-        const unsubscribe = auth.onAuthStateChanged((user) => {
-            console.log(user)
-            authStore.update((curr) => {
-                return { ...curr, isLoading: false, currentUser: user };
-            });
-        });
-    });
-  
-</script>
-
 <slot />
