55686e7bf2642284a677986001e5a379c5a2e710
Mamaril-Jeremy <jmamaril@nevada.unr.edu>
2023-11-27 20:54:22 -0800

    new

--- plato/src/lib/components/Home.svelte
+++ plato/src/lib/components/Home.svelte
@@ -1,201 +1,201 @@
 <script>
-  import Logo  from '$lib/assets/plato_logo.png';
-  import { onMount } from "svelte";
-
-  let text = "Welcome to Plato!";
-  let animatedText = "";
-
-  onMount(() => {
-    const typeAnimation = (text, speed) => {
-      let index = 0;
-
-      const intervalId = setInterval(() => {
-        animatedText = text.slice(0, index);
-        index++;
-
-        if (index > text.length) {
-          clearInterval(intervalId);
-        }
-      }, speed);
-    };
-
-    typeAnimation(text, 70);
-  });
-</script>
-
-<body>
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-<div class="section1">
-  <div>
-    <h1>{animatedText}</h1>
-    <p>We establish mentor to mentee relationships.</p>
-    <img src={Logo} alt="Logo">
+    import Logo  from '$lib/assets/plato_logo.png';
+    import { onMount } from "svelte";
+  
+    let text = "Welcome to Plato!";
+    let animatedText = "";
+  
+    onMount(() => {
+      const typeAnimation = (text, speed) => {
+        let index = 0;
+  
+        const intervalId = setInterval(() => {
+          animatedText = text.slice(0, index);
+          index++;
+  
+          if (index > text.length) {
+            clearInterval(intervalId);
+          }
+        }, speed);
+      };
+  
+      typeAnimation(text, 70);
+    });
+  </script>
+  
+  <body>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+  <div class="section1">
+    <div>
+      <h1>{animatedText}</h1>
+      <p>We establish mentor to mentee relationships.</p>
+      <img src={Logo} alt="Logo">
+    </div>
   </div>
-</div>
-
-  <div class="section2">
-      <div>
-        <h1>76% of people think mentors are important</h1>
-        <p>But only 37% of people currently have one.</p>
-      </div>
-  </div>
-
-  <div class="section3">
-      <div>
-        <h1>ENTER PLATO</h1>
-        <p>We believe in establishing mentor to mentee relationships.</p>
-      </div>
+  
+    <div class="section2">
+        <div>
+          <h1>76% of people think mentors are important</h1>
+          <p>But only 37% of people currently have one.</p>
+        </div>
     </div>
   
-    <div class="section4">
-      <div>
-        <span class="material-icons">mode_comment</span>
-        <h1>Chat</h1>
-        <p>with your connections.</p>
+    <div class="section3">
+        <div>
+          <h1>ENTER PLATO</h1>
+          <p>We believe in establishing mentor to mentee relationships.</p>
+        </div>
       </div>
-      <div>
-        <span class="material-icons">person_add</span>
-        <h1>Connect</h1>
-        <p>with your next potential mentor.</p>
+    
+      <div class="section4">
+        <div>
+          <span class="material-icons">mode_comment</span>
+          <h1>Chat</h1>
+          <p>with your connections.</p>
+        </div>
+        <div>
+          <span class="material-icons">person_add</span>
+          <h1>Connect</h1>
+          <p>with your next potential mentor.</p>
+        </div>
+        <div>
+          <span class="material-symbols-outlined">person_play</span>
+          <h1>Participate</h1>
+          <p>in engaging weekly events.</p>
+        </div>
       </div>
-      <div>
-        <span class="material-symbols-outlined">person_play</span>
-        <h1>Participate</h1>
-        <p>in engaging weekly events.</p>
+    
+      <div class="section5">
+        <div>
+          <h1>Our strongest values are <strong>trust</strong> and <strong>community</strong>.</h1>
+          <p>Ready to get started?</p>
+          <button>Join Now</button>
+        </div>
+        <div></div>
       </div>
-    </div>
+    
+      <div class="section6">
+        <div>
+          <h1>Contact Us</h1>
+          <p>Reach out to us for any inquiries or collaborations.</p>
+        </div>
+        <div></div>
+      </div>
+  </body>
   
-    <div class="section5">
-      <div>
-        <h1>Our strongest values are <strong>trust</strong> and <strong>community</strong>.</h1>
-        <p>Ready to get started?</p>
-        <button>Join Now</button>
-      </div>
-      <div></div>
-    </div>
+  <style>
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: Arial, sans-serif;
+  }
   
-    <div class="section6">
-      <div>
-        <h1>Contact Us</h1>
-        <p>Reach out to us for any inquiries or collaborations.</p>
-      </div>
-      <div></div>
-    </div>
-</body>
-
-<style>
-body {
-  margin: 0;
-  padding: 0;
-  font-family: Arial, sans-serif;
-}
-
-.section1,
-.section2,
-.section3,
-.section4,
-.section5,
-.section6 {
-  overflow: hidden;
-  height: 400px;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  white-space: nowrap;
-  padding: 20px;
-  box-sizing: border-box;
-  text-align: center;
-  width: 100%;
-}
-
-.section1 {
-  height: 600px;
-  background-color: #3498db;
-  color: #fff;
-}
-
-.section2 {
-  background-color: #FEF9FF;
-  color: #3498db;
-
-  height: 400px;
-}
-
-.section3 {
-  background-color: #FEF9FF;
-  color: #3498db;
-  flex-direction: row;
-}
-
-
-.section4 {
-  background-color: #FEF9FF;
-  color: #3498db;
-  flex-direction: row;
-}
-
-.section4 div {
-  flex: 1; 
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-
-.section5{
-  background-color: #3498db;
-  color: #fff;
-  flex-direction: row;
-  height: 700px;
-}
-
-
-h1 {
-  font-size: 48px;
-  font-family: Georgia;
-  margin-bottom: 10px;
-}
-
-p {
-  font-size: 25px;
-  margin-top: 25px;
-  position: relative;
-}
-
-img{
-  justify-content: center;
-  width: 200px;
-  align-items: center;
-  text-align: center;
-  margin: 0 auto;
-  margin-top: 20px;
-}
-
-button {
-  background-color: white;
-  border: none;
-  color: #3498db;
-  padding: 15px 32px;
-  text-align: center;
-  font-size: 16px;
-  margin: 20px auto;
-  cursor: pointer;
-  border-radius: 12px;
-}
-
-.material-icons {
-  font-size: 40px;
-  margin: 0 10px;
-}
-
-.material-symbols-outlined {
-  font-size: 40px;
-  font-variation-settings:
-  'FILL' 0,
-  'wght' 400,
-  'GRAD' 0,
-  'opsz' 24
-}
-</style>
+  .section1,
+  .section2,
+  .section3,
+  .section4,
+  .section5,
+  .section6 {
+    overflow: hidden;
+    height: 400px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    white-space: nowrap;
+    padding: 20px;
+    box-sizing: border-box;
+    text-align: center;
+    width: 100%;
+  }
+  
+  .section1 {
+    height: 600px;
+    background-color: #3498db;
+    color: #fff;
+  }
+  
+  .section2 {
+    background-color: #FEF9FF;
+    color: #3498db;
+  
+    height: 400px;
+  }
+  
+  .section3 {
+    background-color: #FEF9FF;
+    color: #3498db;
+    flex-direction: row;
+  }
+  
+  
+  .section4 {
+    background-color: #FEF9FF;
+    color: #3498db;
+    flex-direction: row;
+  }
+  
+  .section4 div {
+    flex: 1; 
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  
+  .section5{
+    background-color: #3498db;
+    color: #fff;
+    flex-direction: row;
+    height: 700px;
+  }
+  
+  
+  h1 {
+    font-size: 48px;
+    font-family: Georgia;
+    margin-bottom: 10px;
+  }
+  
+  p {
+    font-size: 25px;
+    margin-top: 25px;
+    position: relative;
+  }
+  
+  img{
+    justify-content: center;
+    width: 200px;
+    align-items: center;
+    text-align: center;
+    margin: 0 auto;
+    margin-top: 20px;
+  }
+  
+  button {
+    background-color: white;
+    border: none;
+    color: #3498db;
+    padding: 15px 32px;
+    text-align: center;
+    font-size: 16px;
+    margin: 20px auto;
+    cursor: pointer;
+    border-radius: 12px;
+  }
+  
+  .material-icons {
+    font-size: 40px;
+    margin: 0 10px;
+  }
+  
+  .material-symbols-outlined {
+    font-size: 40px;
+    font-variation-settings:
+    'FILL' 0,
+    'wght' 400,
+    'GRAD' 0,
+    'opsz' 24
+  }
+  </style>
--- plato/src/routes/about/+page.svelte
+++ plato/src/routes/about/+page.svelte
@@ -1,84 +1,121 @@
+<script>
+    
+</script>
+
 <style>
-  /* Base styles for the About page */
-  .about-page {
-      font-size: 16px;
-      line-height: 1.5;
-      margin: 0 auto; 
-      max-width: 800px; 
-      padding: 20px; 
-      text-align: justify; 
-  }
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: Arial, sans-serif;
+    }
 
-  h1, h2 {
-    font-size: 24px;
-    margin-bottom: 10px;
-    text-align: center; 
-  }
+    .section {
+        height: 300px;
+        background-color: #3498db;
+        color: #fff;
+        text-align: center;
+        display: flex;
+        flex-direction: row;
+        font-size: 36px;
+        border-top: 2px solid #000;
+    }
 
-  .about-page .gradient-text-blue:hover {
-      transform: scale(1.05); 
-      cursor: pointer; 
-  }
+    .section1 {
+        overflow: hidden;
+        height: 250px;
+    }
 
-  .gradient-text {
-      background: linear-gradient(rgb(80, 101, 168), rgb(11, 20, 119));
-      -webkit-background-clip: text;
-      background-clip: text;
-      color: transparent;
-  }
+    .section1 > div {
+        height: 250px;
+        background-color: #fff;
+        color: #3498db;
+        text-align: center;
+        display: flex;
+        flex-direction: row;
+        font-size: 36px;
+        border-bottom: 2px solid #000;
+        border-top: 2px solid #000;
+        width: 100%;
+        justify-content: center;
+        align-items: center;
+        border-right: 2px solid #000;
+        white-space: nowrap;
+    }
 
-  .gradient-text-blue {
-      background: linear-gradient(rgb(73, 131, 250), rgb(58, 103, 193));
-      -webkit-background-clip: text;
-      background-clip: text;
-      color: transparent;
-  }
+    .section > div {
+        width: 25%;
+        padding: 20px;
+        box-sizing: border-box;
+    }
+
+    .section > div:nth-child(1) {
+        background-color: #3498db;
+    }
+
+    .section > div:nth-child(2) {
+        width: 75%;
+        background-color: #2ecc71;
+    }
+
+    h1 {
+        margin-bottom: 20px;
+        font-size: 48px;
+    }
+
+    p {
+        font-size: 20px;
+        margin-top: 50px;
+        position: absolute;
+    }
 </style>
 
-<main class="w-screen">
-  <div class="about-page">
-    <!-- Hero Section -->
-    <br>
-    <br>
-  
-    <br>
-  
-    <!-- Hero Section -->
-    <br>
-    <h1>Welcome to <a href="/home" class="gradient-text-blue">Plato</a>, the platform that opens the doors to your personal and professional growth journey.</h1>
-    <br>
-    <p>We're not just another networking site; we are your trusted partner in connecting you with the wisdom and guidance you need to thrive in your chosen field on a mentor-mentee basis.</p>
-    <br>
+<body>
+    <div class="section">
+        <div>
+            <h1>Section 1</h1>
+            <p>This is the first section of our scrolling website.</p>
+        </div>
+        <div>
+            yo
+        </div>
+    </div>
     
-    <!-- "Why 'Plato'?" Section -->
-    <h2><strong class="gradient-text">Why "Plato"?</strong></h2>
-    <br>
-    <p>In the grand tradition of the ancient philosopher <a href="/home" class="gradient-text-blue">Plato</a>, who artfully navigated the intricacies of life's profound inquiries while guiding his devoted disciples, imagine having a seasoned mentor by your side, guiding you through the twists and turns of your career or academic journey. At <a href="/home" class="gradient-text-blue">Plato</a>, mentorship isn't just a feature; it's at the heart of what we do. We believe that the right mentor can be your compass, helping you navigate the path to success. Whether you're a student aiming to conquer challenging subjects or a professional seeking to excel in your industry, we've got you covered.</p>
-    <br>
-    
-    <!-- "Connecting Dreams and Expertise" Section -->
-    <h2><strong class="gradient-text">Connecting Dreams and Expertise</strong></h2>
-    <br>
-    <p><a href="/home" class="gradient-text-blue">Plato</a> is where dreams meet expertise. We're the bridge that brings together passionate learners and seasoned professionals, creating a dynamic synergy that propels you towards greatness. No more wondering where to find the right guidance or feeling lost in a sea of information. Here, you will find mentors who have walked the path you're treading eager to share their wisdom with you.</p>
-    <br>
-    
-    <!-- "Personalized Mentorship" Section -->
-    <h2><strong class="gradient-text">Personalized Mentorship</strong></h2>
-    <br>
-    <p>What sets <a href="/home" class="gradient-text-blue">Plato</a> apart is our unrelenting commitment to the art of personalization. We curate connections based on your unique goals, interests, and aspirations. Our cutting-edge algorithm ensures that you are paired with mentors who possess an intimate understanding of your specific needs, capable of providing tailored, precise support, much akin to a mentor who not only speaks your language but also understands your ambitions in intricate detail.</p>
-    <br>
-    
-    <!-- "Trust" Section -->
-    <h2><strong class="gradient-text">Trust</strong></h2>
-    <br>
-    <p><a href="/home" class="gradient-text-blue">Trust</a> is the cornerstone of <a href="/home" class="gradient-text-blue">Plato</a>. We have painstakingly constructed a secure and transparent platform that not only safeguards your privacy but also guarantees the authenticity of our users. Say farewell to the specter of impersonation and fraudulent actors; <a href="/home" class="gradient-text-blue">Plato</a> stands as your sanctuary for forging genuine and meaningful connections.</p>
-    <br>
-    
-    <!-- "Join the Plato Community Today" Section -->
-    <h2><strong class="gradient-text">Join the Plato Community Today</strong></h2>
-    <br>
-    <p>Enter the thriving community of learners and mentors. Whether you are a student eager to conquer the most formidable academic challenges or a professional steadfastly committed to propelling your career to loftier heights, <a href="/home" class="gradient-text-blue">Plato</a> is your gateway to unlocking untapped reservoirs of potential.</p>
-    <br>
-    <p>Start your journey today and discover how mentorship can propel you to the pinnacles of success. Your voyage towards success commences here, within the ambitions, mentorship, and trust of <a href="/home" class="gradient-text-blue">Plato</a>.</p>
-  </div>
-</main>
+    <div class="section1">
+        <div>
+            <h1>76% of people think mentors are important</h1>
+            <p>x</p>
+        </div>
+    </div>
+
+
+
+    <div class="section">
+        <div>
+            <h1>Section 2</h1>
+            <p>Explore amazing content in this section.</p>
+        </div>
+        <div>
+            yo
+        </div>
+    </div>
+
+    <div class="section">
+        <div>
+            <h1>Section 3</h1>
+            <p>Learn more about our services here.</p>
+        </div>
+        <div>
+            yo
+        </div>
+    </div>
+
+    <div class="section">
+        <div>
+            <h1>Contact Us</h1>
+            <p>Reach out to us for any inquiries or collaborations.</p>
+        </div>
+        <div>
+            yo
+        </div>
+    </div>
+</body>
