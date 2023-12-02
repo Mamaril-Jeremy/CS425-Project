7f8d82e73d90375f1eb60f83dfe9dfd30e0c4439
Mamaril-Jeremy <jmamaril@nevada.unr.edu>
2023-12-01 22:19:23 -0800

    working on home page and fixed coloration

--- plato/src/routes/about/+page.svelte
+++ plato/src/routes/about/+page.svelte
@@ -4,7 +4,7 @@
 
 
 <body>
-    <div class="section">
+    <div class="section section1">
         <div>
             <h1>Section 1</h1>
             <p>This is the first section of our scrolling website.</p>
@@ -14,26 +14,28 @@
         </div>
     </div>
     
-    <div class="section section1">
+    <div class="section section2">
         <div>
             <h1>76% of people think mentors are important</h1>
             <p>x</p>
         </div>
+        <div>
+            yo
+        </div>  
     </div>
 
 
-
-    <div class="section">
+    <div class="section section3">
         <div>
             <h1>Section 2</h1>
             <p>Explore amazing content in this section.</p>
         </div>
-        <div>
+        <div>       
             yo
         </div>
     </div>
 
-    <div class="section">
+    <div class="section section4">
         <div>
             <h1>Section 3</h1>
             <p>Learn more about our services here.</p>
@@ -43,7 +45,7 @@
         </div>
     </div>
 
-    <div class="section">
+    <div class="section section5">
         <div>
             <h1>Contact Us</h1>
             <p>Reach out to us for any inquiries or collaborations.</p>
@@ -72,34 +74,47 @@
         border: 2px solid black;
     }
 
-    .section1 {
-        overflow: hidden;
-    }
-
     .section > div {
-        width: 25%;
-        padding: 20px;
         box-sizing: border-box;
     }
 
-    .section > div:nth-child(1) {
+    .section1,
+    .section3,
+    .section5 > div:nth-child(1) {
+        width: 25%;
         background-color: #3498db;
         border-right: black;
     }
 
-    .section > div:nth-child(2) {
+    .section1,
+    .section3,
+    .section5 > div:nth-child(2) {
+        width: 75%;
+        background-color: #2ecc71;
+    }
+
+    
+    .section2,
+    .section4 > div:nth-child(1) {
+        width: 25%;
+        background-color: #3498db;
+        border-right: black;
+    }
+
+    .section2,
+    .section4 > div:nth-child(2) {
         width: 75%;
         background-color: #2ecc71;
     }
 
     h1 {
-        margin-bottom: 20px;
-        font-size: 48px;
+        margin-top: 15px;
+        font-size: 36px;
     }
 
     p {
         font-size: 20px;
-        margin-top: 50px;
+        margin-top: 20px;
         position: absolute;
     }
 </style>
