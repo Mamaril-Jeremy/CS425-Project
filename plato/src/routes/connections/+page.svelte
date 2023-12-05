009c1cea6af30143dc67b86c7d4a1856442bb4de
Mamaril-Jeremy <jmamaril@nevada.unr.edu>
2023-12-04 21:55:34 -0800

    getting their with database but not 100%

--- plato/src/routes/connections/+page.svelte
+++ plato/src/routes/connections/+page.svelte
@@ -1,16 +1,49 @@
 <script>
     import { Button } from 'flowbite-svelte';
     import Card from '$lib/components/Card.svelte';
+    import { onAuthStateChanged } from 'firebase/auth';
+    import { collection, setDoc, doc } from 'firebase/firestore';
+    import { auth, db } from '$lib/firebase/firebase.client.js';
+
+    let userUID;
+    let userConnectsRemaining = 5; // Set your initial values
+    let userPassesRemaining = 10; // Set your initial values
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            userUID = user.uid;
+        }
+    });
+
+    const updateLimit = async (e) => {
+        if (!userUID) {
+            console.error('User not authenticated');
+            return;
+        }
+
+        const userDocRef = doc(db, "users", userUID);
+
+        try {
+            await setDoc(userDocRef, {
+                userConnectsRemaining: userConnectsRemaining - 1,
+                userPassesRemaining: userPassesRemaining - 1,
+            }, { merge: true });
+
+            console.log("Document updated successfully");
+        } catch (error) {
+            console.error("Error updating document: ", error);
+        }
+    }
 </script>
 
 <div class="pass">
-    <Button color="red" class="ml-100">Pass</Button>
+    <Button color="red" class="ml-100" on:click={updateLimit}>Pass</Button>
 </div>
 <div class="container">
     <Card />
 </div>
 <div class="connect">
-    <Button color="green" class="mr-100">Connect</Button> 
+    <Button color="green" class="mr-100" on:click={updateLimit}>Connect</Button>
 </div>
 
 <style>
@@ -21,6 +54,7 @@
         justify-content: center;
         height: 100vh;
     }
+
     .pass {
         margin-left: 450px;
     }
--- plato/src/routes/profile/+page.svelte
+++ plato/src/routes/profile/+page.svelte
@@ -1,11 +1,12 @@
 <script>
-  import { collection, addDoc } from 'firebase/firestore';
-  import { Avatar, Label, Input, GradientButton, Checkbox, A } from 'flowbite-svelte';
+  import { collection, setDoc, getDoc, doc } from 'firebase/firestore';
+  import { Avatar, Label, Input, GradientButton } from 'flowbite-svelte';
   import { onAuthStateChanged } from 'firebase/auth';
   import { auth, db } from '$lib/firebase/firebase.client.js';
   import Pfp from "$lib/assets/Mark Marsala.jpg";
 
-  let userUID, firstName, lastName, email, phoneNumber, occupation, role, connectsRemaining, passesRemaining;
+  let userUID, firstName, lastName, email, phoneNumber, occupation, role, connectsRemaining = 5, passesRemaining = 10;
+  let localFirstName, localLastName, localEmail, localPhoneNumber, localOccupation, localRole;
 
   onAuthStateChanged(auth, (user) => {
     if (user) {
@@ -13,17 +14,44 @@
     }
   });
 
+  const fetchData = async () => {
+    const docRef = doc(db, "users", userUID);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      lastName = data.userLastName
+      emailAddress = data.userEmailAddress
+      occupation = data.userOccupation
+      role = data.userRole
+      connectsRemaining = data.userConnectsRemaining
+      phoneNumber = data.userPhoneNumber
+      passesRemaining = data.userPassesRemaining
+      userUID = data.userID
+      firstName = data.userFirstName
+
+    } else {
+      console.log("No such document!");
+    }
+  };
+
   const handleClick = async (e) => {
     if (!userUID) {
       console.error('User not authenticated');
       return;
     }
 
-    const docRef = await addDoc(collection(db, "users"), {
+    firstName = localFirstName;
+    lastName = localLastName;
+    email = localEmail;
+    phoneNumber = localPhoneNumber;
+    occupation = localOccupation;
+    role = localRole;
+
+    const docRef = await setDoc(collection(db, "users", userUID), {
       userLastName: lastName,
       userEmailAddress: email,
       userOccupation: occupation,
-      userRole: role.toLowerCase() === 'mentor',
+      userRole: role.toLowerCase(),
       userConnectsRemaining: connectsRemaining,
       userPhoneNumber: phoneNumber,
       userPassesRemaining: passesRemaining,
@@ -32,6 +60,8 @@
     });
     console.log("Document written with ID: ", docRef.id);
   };
+
+  $: fetchData();
 </script>
 
 <style>
@@ -49,7 +79,7 @@
 
   .form-container {
     position: relative;
-    margin-top: 150px;
+    margin-top: 200px;
   }
 
   .centered-button {
@@ -57,6 +87,12 @@
     justify-content: center;
     align-items: center;
     margin: 25px auto;
+  }
+
+  .constants{
+    display: flex;
+    margin: 25px auto;
+    font-size: 20px;
   }
 
   body {
@@ -74,7 +110,7 @@
       <Avatar src="{Pfp}" data-name="Mark Marsala" border class="ring-blue-600 dark:ring-blue-300" size="lg"
         dot={{ placement: 'top-right', color: 'green', size: 'lg' }} />
       <div class="space-y-1 font-medium dark:text-black">
-        <div>Mark Marsala</div>
+        <div>{firstName} {lastName}</div>
         <div class="text-sm text-gray-500 dark:text-gray-400">Joined in September 2023</div>
       </div>
     </div>
@@ -85,37 +121,41 @@
       <div class="grid gap-6 mb-6 md:grid-cols-2">
         <div>
           <Label for="first_name" class="mb-2">First name</Label>
-          <Input type="text" id="first_name" placeholder="First" bind:value={firstName} required />
+          <Input type="text" id="first_name" placeholder="First" bind:value={localFirstName} required />
         </div>
         <div>
           <Label for="last_name" class="mb-2">Last name</Label>
-          <Input type="text" id="last_name" placeholder="Last" bind:value={lastName} required />
+          <Input type="text" id="last_name" placeholder="Last" bind:value={localLastName} required />
         </div>
         <div>
           <Label for="company" class="mb-2">Occupation</Label>
-          <Input type="text" id="company" placeholder="Plato" bind:value={occupation} required />
+          <Input type="text" id="company" placeholder="Plato" bind:value={localOccupation} required />
         </div>
         <div>
           <Label for="phone" class="mb-2">Phone number</Label>
           <Input type="tel" id="phone" placeholder="123-456-7890"
-            bind:value={phoneNumber} required />
+            bind:value={localPhoneNumber} required />
         </div>
         <div>
           <Label for="role" class="mb-2">Role</Label>
-          <Input type="text" id="role" placeholder="mentor/mentee" bind:value={role} required />
+          <Input type="text" id="role" placeholder="mentor/mentee" bind:value={localRole} required />
+        </div>
+        <div>
+          <Label for="role" class="mb-2">Role</Label>
+          <Input type="text" id="role" placeholder="mentor/mentee" required />
         </div>
         <div>
           <Label for="visitors" class="mb-2">Connects Remaining</Label>
-          <Input type="number" id="visitors" placeholder="" bind:value={connectsRemaining} required />
+          <div class="constants">{connectsRemaining}</div>
+        </div>
+        <div>
+          <Label for="visitors" class="mb-2">Passes Remaining</Label>
+          <div class="constants">{passesRemaining}</div>
         </div>
       </div>
       <div>
-        <Label for="passes" class="mb-2">Passes Remaining</Label>
-        <Input class="mb-3" type="number" id="passes" placeholder="" bind:value={passesRemaining} required />
-      </div>
-      <div>
         <Label for="email" class="mb-2">Email</Label>
-        <Input class="mb-3" type="email" id="email" placeholder="name@domain.com" bind:value={email} required />
+        <Input class="mb-3" type="email" id="email" placeholder="name@domain.com" bind:value={localEmail} required />
       </div>
       <div class="centered-button">
         <GradientButton type="submit" color="purpleToBlue">Submit</GradientButton>
