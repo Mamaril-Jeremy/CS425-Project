39c1d65ddd8191e79d19e3e6806234445feb281a
Mamaril-Jeremy <jmamaril@nevada.unr.edu>
2024-02-05 20:52:59 -0800

    added some minor frontend details

--- plato/src/lib/components/Home.svelte
+++ plato/src/lib/components/Home.svelte
@@ -48,7 +48,7 @@
     </div>
     <div>
       {#if fadeIn}
-      <p in:fade={{delay: 5, duration: 1600}}>Establishing mentor to mentee relationships</p>
+      <p in:fade={{delay: 5, duration: 1600}}><strong>We're committed to establishing mentor-to-mentee relationships.</strong></p>
       {/if}
     </div>
     <video autoplay loop muted>
