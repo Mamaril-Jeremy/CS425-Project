7bc8daa266e309e92326b43dbbff1cdee3ab9f92
markmarsala <markmarsala@nevada.unr.edu>
2024-03-02 15:29:47 -0800

    ReCAPTCHA created

--- plato/src/routes/create-account/+layout.svelte
+++ plato/src/routes/create-account/+layout.svelte
@@ -1,1 +1,13 @@
 <slot />
+
+<script>
+  import { onMount } from 'svelte';
+  
+  onMount(() => {
+    const script = document.createElement('script');
+    script.src = 'https://www.google.com/recaptcha/api.js';
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+  });
+</script>
